e771c8ef5a33862f9b99e5f17d592d0a05a26bb1
iMikhaylove <104492998+iMikhaylove@users.noreply.github.com>
2022-10-31 17:32:21 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import React from "react";
+import axios from "axios";
+import Card from "./components/Card";
+import Header from "./components/Header";
+import Drawer from "./components/Drawer";
+import {Routes, Route} from 'react-router-dom';
+
+function App() {
+  const [items, setItems] = React.useState([]);
+  const [cartItems, setCartItems] = React.useState([]);
+  const [favorites, setFavorites] = React.useState([]);
+  const [searchValue, setSearchValue] = React.useState('');
+  const [cartOpened, setCartOpened] = React.useState(false);
+
+  React.useEffect(() =>{
+    axios.get('https://6359f4aaff3d7bddb9abf23b.mockapi.io/items/items').then(res => {
+      setItems(res.data)
+    });
+    axios.get('https://6359f4aaff3d7bddb9abf23b.mockapi.io/items/items').then(res => {
+      setCartItems(res.data)
+    });
+  }, []);
+  
+  const onAddToCart = (obj) => {
+    axios.post('https://6359f4aaff3d7bddb9abf23b.mockapi.io/items/cart', obj)
+    setCartItems(prev => [... prev, obj]);
+  }
+
+  const onRemoveItem = (id) => {
+    axios.delete(`https://6359f4aaff3d7bddb9abf23b.mockapi.io/items/cart${id}`)
+    setCartItems((prev) => prev.filter((item) => item.id !== id))
+  }
+
+  const onAddToFavorite = (obj) => {
+    axios.post('https://6359f4aaff3d7bddb9abf23b.mockapi.io/items/favorites', obj)
+    setFavorites(prev => [... prev, obj]);
+  }
+
+  const onChangeSearchInput = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  return (
+    <div className="wrapper clear">
+      {cartOpened && <Drawer items={cartItems} onClose={() => setCartOpened(false)} onRemove={onRemoveItem} />}
+      <Header onClickCart={() => setCartOpened(true)} />
+      <Routes>
+ <Route  path="/favorites"  />                
+        </Routes>
+
+      <div className="content p-40">
+        <div className="d-flex align-center justify-between mb-40">
+          <h1>{searchValue ? `Поиск по запросу: "${searchValue}"` : 'Все кроссовки'}</h1>
+          <div className="search-block d-flex">
+            <img src="/img/search.svg" alt="Search" />
+            {searchValue && <img 
+            onClick={() => setSearchValue('')} 
+            className="clear cu-p" 
+            src="/img/btn-remove.svg" 
+            alt="Clear" />}
+            <input onChange={onChangeSearchInput} value={searchValue} placeholder="Поиск ..." />
+          </div>
+        </div>
+
+        <div className="d-flex flex-wrap">
+          {items.filter((item) => item.title.toLowerCase()
+          .includes(searchValue.toLowerCase()))
+          .map((item, index) =>(
+            <Card
+            key={index} 
+            title={item.title}
+            price={item.price}
+            imageUrl={item.imageUrl}
+            onFavorite={(obj) =>onAddToFavorite(obj)}
+            onClick={(obj) => onAddToCart(obj)}
+          />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
